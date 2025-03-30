169f2fa28f0f61e8941cc73d22e4129e01def323
Shangwen Wang <Wang.shangwen@outlook.com>
2025-03-30 12:03:27 +0200

    Demo 0.1 with basic player controller and simple scene.

---
+++ Assets/Script/PlayerBController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerBController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 8f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float move = 0f;
+        if (Input.GetKey(KeyCode.LeftArrow)) move = -1f;
+        if (Input.GetKey(KeyCode.RightArrow)) move = 1f;
+
+        rb.linearVelocity = new Vector2(move * moveSpeed, rb.linearVelocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+}
