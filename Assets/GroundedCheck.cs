18ee85390f10c6a53dfb3bf2ba63b4ba18fb5aac
William Koivula <wkoivula2001@gmail.com>
2025-05-19 11:23:34 +0200

    Fixed bug where jump animation was reset when jumping upwards

--- Assets/GroundedCheck.cs
+++ Assets/GroundedCheck.cs
@@ -5,8 +5,18 @@ public class GroundedCheck : MonoBehaviour
     public Animator animator;
     public Player player;
 
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponentInParent<Rigidbody>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        animator.SetBool("isJumping", false);
+        if (rb.linearVelocity.y < 0)
+        {
+            animator.SetBool("isJumping", false);
+        }
     }
 }
